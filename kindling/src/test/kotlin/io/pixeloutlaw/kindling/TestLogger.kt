fec0c8b3a093e1dcd8a37ca350671fd2b67ffd19
Richard Harrah <topplethenunnery@gmail.com>
2024-07-05 19:12:15 -0400

    update to java 21 and mdgp 9

--- kindling/src/test/kotlin/io/pixeloutlaw/kindling/TestLogger.kt
+++ kindling/src/test/kotlin/io/pixeloutlaw/kindling/TestLogger.kt
@@ -3,7 +3,9 @@ package io.pixeloutlaw.kindling
 /**
  * Logger implementation used for testing the logic in [Logger].
  */
-class TestLogger(override val minimumLogLevel: Log.Level) : Logger() {
+class TestLogger(
+    override val minimumLogLevel: Log.Level,
+) : Logger() {
     val loggedMessages = mutableListOf<LogRecord>()
 
     override fun print(logRecord: LogRecord) {
